1e5c7abaf711541d4719b22b6780e2e82e7dacca
fan1138612367 <fan1138612367@vip.qq.com>
2021-05-22 21:59:05 +0800

    第二天

---
+++ src/domain/CDC.java
@@ -0,0 +1,49 @@
+package domain;
+
+public class CDC {
+    private String time;
+    private String college;
+    private String department;
+    private String cclass;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getCclass() {
+        return cclass;
+    }
+
+    public void setCclass(String cclass) {
+        this.cclass = cclass;
+    }
+
+    @Override
+    public String toString() {
+        return "CDC{" +
+                "college='" + college + '\'' +
+                ", department='" + department + '\'' +
+                ", cclass='" + cclass + '\'' +
+                '}';
+    }
+}
